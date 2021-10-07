4a0c88a740fba96ba09e84448d468158c5ff1694
lerausenco <70701722+lerausenco@users.noreply.github.com>
2021-10-07 11:31:03 +0200

    Added direction vectors

--- Shapes/Block.py
+++ Shapes/Block.py
@@ -10,7 +10,7 @@ class Block:
     def getVolume(self):
         return self.length * self.width * self.height
 
-    def __init__(self, x, y, z, length, width, height, direction=[0, 0, 1], color='RED', material='Steel'):
+    def __init__(self, x, y, z, length, width, height, xdirection=[1, 0, 0], ydirection=[0,1,0], color='RED', material='Steel'):
         self.x = x
         self.y = y
         self.z = z
@@ -18,7 +18,9 @@ class Block:
         self.width = width
         self.height = height
 
-        self.direction = direction
+        self.xdirection = NXOpen.Vector3d(float(xdirection[0]), float(xdirection[1]),float(xdirection[2]))
+													 
+        self.ydirection = NXOpen.Vector3d(float(ydirection[0]), float(ydirection[1]), float(ydirection[2]))
         self.color = color
         self.material = material
         
@@ -35,6 +37,8 @@ class Block:
         origBlock = NXOpen.Point3d(float(self.x), float(self.y), float(self.z))
         blockfeaturebuilder1.SetOriginAndLengths(origBlock, str(self.length), str(self.width), str(self.height))
         blockfeaturebuilder1.BooleanOption.Type = NXOpen.GeometricUtilities.BooleanOperation.BooleanType.Create
+        blockfeaturebuilder1.SetOrientation(self.xdirection, self.ydirection)
+
 
         self.body = blockfeaturebuilder1.Commit().GetBodies()[0]
         blockfeaturebuilder1.Destroy()
